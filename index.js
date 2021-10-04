60ecf0f82cdbbd6aa4fa22c36f1e90fc2920125d
suditya <70383984+suditya@users.noreply.github.com>
2021-10-04 13:28:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+    // console.log("harry bhai");
+    d =new Date();
+    htime= d.getHours();
+    mtime= d.getMinutes();
+    stime =d.getSeconds();
+    hrotation= 30*(htime)+ (mtime/2); 
+    mrotation= 6*mtime;
+    srotation= 6*stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    min.style.transform=`rotate(${mrotation}deg)`;
+    sec.style.transform=`rotate(${srotation}deg)`;
+    // hour.style.transform = `rotate(${hrotation}deg)`;
+    // min.style.transform = `rotate(${mrotation}deg)`;
+    // sec.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
